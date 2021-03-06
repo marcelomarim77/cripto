0cd99a1a518ffe38cba38b632ddb9ddc2cf25d36
marcelovm <marcelo.marim@hdi.com.br>
2021-03-06 12:11:07 -0300

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CryptService } from './crypt.service';
 
 import { Cripto } from './cripto';
@@ -10,23 +10,18 @@ import { Cripto } from './cripto';
 })
 export class AppComponent {
     @Input() cripto: Cripto;
-    @Input() descripto: Cripto;
 
     constructor(private cryptService: CryptService) {}
 
     getCripto(msg: string): void {
+        if (msg.length == 0) {
+            window.alert('Informe o texto que será criptografado ou descriptografado');
+            return;
+        }
         this.cryptService.getCrypt(msg)
             .subscribe(data => {
                 console.log(data);
                 this.cripto = data;
-         });
-    }
-
-    getDescripto(msg: string): void {
-        this.cryptService.getDecrypt(msg)
-            .subscribe(data => {
-                console.log(data);
-                this.descripto = data;
          });
     }
 }
